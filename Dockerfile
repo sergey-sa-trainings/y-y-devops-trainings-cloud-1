6af3f4dc9d05a74d57bd7c22a65e98f453051ada
sergey-sa <sergey-sa@debian.sergey-sa>
2023-11-15 16:48:51 +0600

    terraform

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,15 @@
+ARG GOARCH=amd64
 # Базовый образ
 FROM golang:1.21 as builder
 
 WORKDIR /go/src/catgpt
-COPY ./catgpt/ .
+COPY ./catgpt .
 
 RUN go mod download
-RUN CGO_ENABLED=0 go build -o /go/bin/catgpt-app
+RUN CGO_ENABLED=0 GOARCH=${GOARCH} go build -o /go/bin/catgpt-app
 
 # Финальный образ
-FROM gcr.io/distroless/static-debian12:latest-amd64
+FROM gcr.io/distroless/static-debian12:latest-${GOARCH}
 COPY --from=builder /go/bin/catgpt-app /
+EXPOSE 8080 9090
 CMD ["/catgpt-app"]
--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "yandex" {
-  service_account_key_file = "./tf_key.json"
+  service_account_key_file = "./sa-terraform-hw2-key.json"
   folder_id                = local.folder_id
   zone                     = "ru-central1-a"
 }
@@ -21,60 +21,132 @@ resource "yandex_vpc_subnet" "foo" {
   v4_cidr_blocks = ["10.5.0.0/24"]
 }
 
-resource "yandex_container_registry" "registry1" {
-  name = "registry1"
+resource "yandex_container_registry" "registry-hw2" {
+  name = "registry-hw2"
+}
+
+output "container_registry_id" {
+  value = yandex_container_registry.registry-hw2.id
 }
 
 locals {
-  folder_id = "<INSERT YOUR FOLDER ID>"
+  folder_id = "b1gi3jo0sam0o77lva2p"
   service-accounts = toset([
-    "catgpt-sa",
+    "catgpt-ig", "catgpt-it"
   ])
-  catgpt-sa-roles = toset([
+  catgpt-ig-roles = toset([
+    "vpc.admin",
+    "vpc.user",
+    "iam.serviceAccounts.user",
+    "load-balancer.admin",
+    "compute.editor"
+  ])
+  catgpt-it-roles = toset([
     "container-registry.images.puller",
     "monitoring.editor",
   ])
 }
 resource "yandex_iam_service_account" "service-accounts" {
   for_each = local.service-accounts
-  name     = each.key
+  name     = "${local.folder_id}-${each.key}"
 }
-resource "yandex_resourcemanager_folder_iam_member" "catgpt-roles" {
-  for_each  = local.catgpt-sa-roles
+resource "yandex_resourcemanager_folder_iam_member" "catgpt-ig-roles" {
+  for_each  = local.catgpt-ig-roles
   folder_id = local.folder_id
-  member    = "serviceAccount:${yandex_iam_service_account.service-accounts["catgpt-sa"].id}"
+  member    = "serviceAccount:${yandex_iam_service_account.service-accounts["catgpt-ig"].id}"
+  role      = each.key
+}
+resource "yandex_resourcemanager_folder_iam_member" "catgpt-it-roles" {
+  for_each  = local.catgpt-it-roles
+  folder_id = local.folder_id
+  member    = "serviceAccount:${yandex_iam_service_account.service-accounts["catgpt-it"].id}"
   role      = each.key
 }
 
 data "yandex_compute_image" "coi" {
   family = "container-optimized-image"
 }
-resource "yandex_compute_instance" "catgpt-1" {
+
+resource "yandex_compute_instance_group" "catgpt-2" {
+  service_account_id = yandex_iam_service_account.service-accounts["catgpt-ig"].id
+  folder_id          = local.folder_id
+  instance_template {
+    service_account_id = yandex_iam_service_account.service-accounts["catgpt-it"].id
     platform_id        = "standard-v2"
-    service_account_id = yandex_iam_service_account.service-accounts["catgpt-sa"].id
     resources {
       cores         = 2
       memory        = 1
       core_fraction = 5
     }
+    metadata = {
+      docker-compose = file("${path.module}/docker-compose.yaml")
+      ssh-keys       = "ubuntu:${file("~/.ssh/y-devops-training.pub")}"
+      user-data = "${file("${path.module}/cloud-config.yaml")}"
+    }
+
+    boot_disk {
+      mode = "READ_WRITE"
+      initialize_params {
+        image_id = data.yandex_compute_image.coi.id
+        type     = "network-hdd"
+        size     = "30"
+      }
+    }
+    network_interface {
+      network_id = yandex_vpc_network.foo.id
+      subnet_ids = [yandex_vpc_subnet.foo.id]
+      nat        = true
+    }
+    network_settings {
+      type = "STANDARD"
+    }
     scheduling_policy {
       preemptible = true
     }
-    network_interface {
-      subnet_id = "${yandex_vpc_subnet.foo.id}"
-      nat = true
+  }
+
+  scale_policy {
+    fixed_scale {
+      size = 2
     }
-    boot_disk {
-      initialize_params {
-        type = "network-hdd"
-        size = "30"
-        image_id = data.yandex_compute_image.coi.id
-      }
-    }
-    metadata = {
-      docker-compose = file("${path.module}/docker-compose.yaml")
-      ssh-keys  = "ubuntu:${file("~/.ssh/devops_training.pub")}"
-    }
+  }
+
+  allocation_policy {
+    zones = ["ru-central1-a"]
+  }
+
+  deploy_policy {
+    max_unavailable = 1
+    max_creating    = 1
+    max_expansion   = 1
+    max_deleting    = 1
+  }
+
+  load_balancer {
+    target_group_name = "catgpt-2"
+  }
 }
 
+resource "yandex_lb_network_load_balancer" "lb-hw2" {
+  name = "lb-hw2"
 
+  listener {
+    name = "my-listener"
+    port = 8080
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+
+  attached_target_group {
+    target_group_id = yandex_compute_instance_group.catgpt-2.load_balancer.0.target_group_id
+
+    healthcheck {
+      name = "http"
+      http_options {
+        port = 8080
+        path = "/ping"
+      }
+    }
+  }
+}
